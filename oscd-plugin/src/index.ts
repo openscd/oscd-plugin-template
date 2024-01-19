943b060acc07d8cdcb598ab7735a7d4f76cc3f9c
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2024-01-19 22:27:53 +0100

    Added Lit Implementation

---
+++ oscd-plugin/src/index.ts
@@ -0,0 +1,1 @@
+export { OscdPlugin } from './OscdPlugin.js';
---
+++ oscd-plugin/stories/index.stories.ts
@@ -0,0 +1,62 @@
+import { html, TemplateResult } from 'lit';
+import { OscdPlugin } from '../src/OscdPlugin.js';
+
+window.customElements.define('oscd-plugin', OscdPlugin);
+
+export default {
+  title: 'OscdPlugin',
+  component: 'oscd-plugin',
+  argTypes: {
+    header: { control: 'text' },
+    counter: { control: 'number' },
+    textColor: { control: 'color' },
+  },
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {
+  header?: string;
+  counter?: number;
+  textColor?: string;
+  slot?: TemplateResult;
+}
+
+const Template: Story<ArgTypes> = ({
+  header = 'Hello world',
+  counter = 5,
+  textColor,
+  slot,
+}: ArgTypes) => html`
+  <oscd-plugin
+    style="--oscd-plugin-text-color: ${textColor || 'black'}"
+    .header=${header}
+    .counter=${counter}
+  >
+    ${slot}
+  </oscd-plugin>
+`;
+
+export const Regular = Template.bind({});
+
+export const CustomHeader = Template.bind({});
+CustomHeader.args = {
+  header: 'My header',
+};
+
+export const CustomCounter = Template.bind({});
+CustomCounter.args = {
+  counter: 123456,
+};
+
+export const SlottedContent = Template.bind({});
+SlottedContent.args = {
+  slot: html`<p>Slotted content</p>`,
+};
+SlottedContent.argTypes = {
+  slot: { table: { disable: true } },
+};
